46179fab9b625ce41f085fe7114f0138c3c90a56
srinivas <srinivas.ece450@gmail.com>
2026-02-18 14:02:02 +1100

    variables giving dynamically hiding password

--- Practice/Shell_scripting/04.variables.sh
+++ Practice/Shell_scripting/04.variables.sh
@@ -2,6 +2,6 @@
 
 echo "Please enter your name:"
 read USER_NAME
-echo "User Name is $USER_NAME"
+echo "User Name is: $USER_NAME"
 read -s PASSWORD
-echo "password is $PASSWORD"
+echo "password is: $PASSWORD"
