7361bb860a9e9eafcd588d9a11e0c5bb545a044c
srinivas <srinivas.ece450@gmail.com>
2026-02-19 18:13:01 +1100

    condition

--- Practice/Shell_scripting/09.conditions.sh
+++ Practice/Shell_scripting/09.conditions.sh
@@ -8,6 +8,6 @@ echo "Given number is: $Number is greater than 20"
 elif[ $Number -eq 20 ]; then
 echo "Given number is: $Number is equal to 20"
 
-else[ $Numebr -lt 20 ]; then
+else[ $Numebr -lt 20 ];
 echo "Given number is: $Number is less than 20"
 fi
