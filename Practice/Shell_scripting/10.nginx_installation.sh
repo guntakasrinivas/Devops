dedc2b4e2c7e4de30f18927baf7c4a135c497e07
srinivas <srinivas.ece450@gmail.com>
2026-02-19 18:25:48 +1100

    condition

---
+++ Practice/Shell_scripting/10.nginx_installation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID= (id-u)
+if [ USERID -ne 0]; then
+echo "Please run the script using root access"
+exit 1
+fi
+
+echo "installing nginx"
+
+dnf install nginx -y
+
+if [ $? -ne 0]; then
+echo "Installing nginx.. Failure"
+
+exit 1
+
+else
+echo "instaling nginx.. Success"
+
+dnf install MySql -y
+
+if [ $? -ne 0]; then
+echo "Installing Mysql.. Failure"
+
+exit 1
+
+else
+echo "Installing Mysql.. Success"
+
+fi
